e3e987d2b8990be28ddb5c1829c5882ce8c2d5da
maximus12793 <maximus12793@gmail.com>
2023-02-23 02:07:03 -0600

    example 1-2

--- base_c/main.c
+++ base_c/main.c
@@ -1,9 +1,21 @@
 #include <stdio.h>
 #include <stdlib.h>
 
-int main() {
+int example_1() {
+  // Example 1 using malloc syntax.
   char * str;
+  // Function allocates memory and leaves the memory uninitialized.
   str = (char*) malloc(15);
+
+  free(str);
+  return(0);
+}
+
+int example_2() {
+  //Example 2 using calloc syntax.
+  char * str;
+  // Function allocates memory and initializes all bits to zero.
+  str = (char*) calloc(15, sizeof(char));
 
   free(str);
   return(0);
